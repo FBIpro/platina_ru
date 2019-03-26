aab7d7f78855554fe050ad7440214e1374c48c2b
FBIpro <48206098+FBIpro@users.noreply.github.com>
2019-03-26 13:39:19 +0600

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/var/www/u0000006/data/platina/')
-sys.path.insert(1, '/var/www/u0000006/data/platina/platina/')
+sys.path.insert(0, '/var/www/u0683760/data/platina/')
+sys.path.insert(1, '/var/www/u0683760/data/platina/platina/')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'platina.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
